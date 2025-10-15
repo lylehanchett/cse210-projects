99085d864d8db23d166f0b1355b09c6bd89fc794
lylehanchett <lylesh381@gmail.com>
2025-10-14 17:06:43 -0700

    week7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("=== Fitness Tracker App ===");
+        Console.WriteLine("Tracking your running, cycling, and swimming progress!\n");
+
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("Nov 3, 2022", 30, 3.0));
+        activities.Add(new Cycling("Nov 3, 2022", 30, 12.0));
+        activities.Add(new Swimming("Nov 3, 2022", 30, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
+        Console.WriteLine("\nProgram finished. Press any key to close.");
+        Console.ReadKey();
     }
 }
